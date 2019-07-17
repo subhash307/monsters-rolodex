d9ee22ec401b2ad4d1d8cff644426ea9fc81690d
Subhash Kumar <subhash.kumar307@gmail.com>
2019-07-17 11:56:07 +0530

    Updates

---
+++ precache-manifest.b4ad4ef412c7e2a936f36282ba7c425a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2ebba34bf90270af4335c26a15247f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5b8d902e7da0dc8bb19a",
+    "url": "/monsters-rolodex/static/css/main.c61f75a7.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "5b8d902e7da0dc8bb19a",
+    "url": "/monsters-rolodex/static/js/main.ae152db4.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
